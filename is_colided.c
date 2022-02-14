fba968ba472434bad8db079fc982cd042392ba34
velvelhype <velvelhype@gmail.com>
2022-02-15 08:56:04 +0900

    uodate:sphere

--- is_colided.c
+++ is_colided.c
@@ -1,7 +1,26 @@
 #include "term3d.h"
+#include "vector_utils.h"
 
-void	is_colided()
+float quadratic_equation(t_vector eye_dir, t_vector obj_to_eye, t_term *info)
+{
+    float A, B, C, D;
+
+    A = squared_norm(&eye_dir);
+    B = 2 * dot(&eye_dir, &obj_to_eye);
+    C = squared_norm(&obj_to_eye) - SQR(info->sphere_r);
+    D = SQR(B) - 4 * A * C;	
+	return (D);
+}
+
+float	is_colided(int x, int y, t_term *info)
 {
 	// calculate eyedir
+	t_vector screen_pos  = {x, y, info->screen_z};
+	t_vector eye_dir = sub(&screen_pos, &(info->eye_pos));
+	t_vector obj_to_eye = sub(&(info->eye_pos), &(info->sphere_pos));
+	//二次方程式
+	return (quadratic_equation(eye_dir, obj_to_eye, info));
+	// printf("%s\n", vector_str(&screen_pos));
+	exit(1);
 	// eyedirと物体が交差しているか判定して出力
 }
--- main.c
+++ main.c
@@ -4,7 +4,14 @@ void	init_info(t_term *info)
 {
 	info->height = 30;
 	info->width = 30;
-	t_vector eye = {0, 0, 0};
+	info->screen_z = -5;
+	info->eye_pos.x = 0;
+	info->eye_pos.y = 0;
+	info->eye_pos.z = -10;
+	info->sphere_pos.x = 0;
+	info->sphere_pos.y = 0;
+	info->sphere_pos.z = 0;
+	info->sphere_r = 9;
 }
 
 int main()
@@ -12,21 +19,22 @@ int main()
 	t_term info;
 
 	init_info(&info);
-	int x = 0;
-	int y = 0;
-	while(y < info.height)
+	int x = -1 * (info.width) / 2;
+	int y = -1 * (info.height) / 2;
+	while(y < info.width / 2)
 	{
-		while(x < info.width)
+		while(x < info.height / 2)
 		{
 			// （x, y）が図形と衝突しているか判定
-			// is_colided(x, y);
-			printf("▩");
-			//
+			if (is_colided(x, y, &info) >= 0)
+				printf("▩");
+			else
+				printf(" ");
 			printf(" ");
 			x++;
 		}
 		printf("\n");
-		x = 0;
+		x = -1 * (info.width) / 2;
 		y++;
 	}
 }
