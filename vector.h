db943c2f5dc6817d0dc5696e4791fde98dad3d86
velvelhype <velvelhype@gmail.com>
2022-02-15 22:00:19 +0900

    vector

---
+++ vector.h
@@ -0,0 +1,31 @@
+#ifndef VECTOR_UTILS_H
+#define VECTOR_UTILS_H
+
+#define SQR(x) ((x)*(x))
+#define MIN(a,b) (a < b ? a : b)
+#define MAX(a,b) (a > b ? a : b)
+#define CLAMP(v,minv,maxv) MIN(MAX(v,minv),maxv)
+
+typedef struct s_vector
+{
+  float x;
+  float y;
+  float z;
+} t_vector;
+
+void  init_vector(t_vector *vec, float arg_x, float arg_y, float arg_z);
+
+t_vector add(const t_vector* a, const t_vector* b);
+t_vector sub(const t_vector* a, const t_vector* b);
+t_vector mult(const t_vector* a, float b);
+
+float dot(const t_vector* a, const t_vector* b);
+void cross(t_vector* o, const t_vector*a, const t_vector*b);
+float squared_norm(const t_vector*v);
+float norm(const t_vector *v);
+float normalize(t_vector* v);
+
+
+const char* vector_str(const t_vector*v);
+
+#endif /* VECTOR_UTILS_H */
