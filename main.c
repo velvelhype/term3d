c111ca1b499418566479e5aa4f92b155aaf21a55
tyamagis @ GCML <tyamagis@student.42tokyo.jp>
2022-02-18 20:32:14 +0900

    refactoring

--- main.c
+++ main.c
@@ -9,68 +9,30 @@ int	exit_me(void)
 
 void	init_info(t_term *info)
 {
-	info->height = 60;
-	info->width = 60;
+	info->height = 60; // GET terminal size
+	info->width = 60; // GET terminal size
+	info->lim_x = info->height * 0.5;
+	info->lim_y = info->width * -0.5;
 	info->screen_z = 0;
 	init_vector(&(info->eye_pos), 0, 0, -50);
 	init_vector(&(info->sphere_pos), 0, 0, 0);
 	info->sphere_r = 10;
 	info->zoom = 8;
 	info->deg = 0;
-}
-
-void	set_char(int *xy, char *data, t_term *tm, t_ply *ply)
-{
-	float	d;
-
-	while (xy[1] > -1 * tm->width / 2)
-	{
-		while (xy[0] < tm->height / 2)
-		{
-			d = is_colided(xy[0], xy[1], tm, ply);
-			if (d >= 0)
-			{
-				memset(data, '+', 1);
-				memset(data + 1, ' ', 1);
-			}
-			else
-				memset(data, ' ', 2);
-			data += 2;
-			xy[0]++;
-		}
-		memset(data, '\n', 1);
-		data++;
-		xy[0] = -1 * (tm->width) / 2;
-		xy[1]--;
-	}
-	memset(data, '\0', 1);
+	return ;
 }
 
 int	main(int argc, char **argv)
 {
 	t_term	tm;
 	t_ply	*ply;
-	char	*data;
-	size_t	datasize;
-	int		xy[2];
 
 	if (argc != 2)
 		exit_me();
 	ply = parse_ply(argv[1]);
+	if (ply == NULL)
+		exit_me();
 	init_info(&tm);
-	xy[0] = -1 * (tm.width) / 2;
-	xy[1] = 1 * (tm.height) / 2;
-	datasize = (tm.width + 1) * (tm.height + 1) + 1;
-	data = (char *)malloc(datasize);
-	while (1)
-	{
-		set_char(xy, data, &tm, ply);
-		fprintf(stderr, "\033[2J\033[2H");
-		fprintf(stderr, "%s", data);
-		if (tm.deg > 360)
-			tm.deg -= 360;
-		tm.deg += 10;
-		xy[1] = 1 * (tm.height) / 2;
-		usleep(50000);
-	}
+	loop_draw(&tm, ply);
+	return (0);
 }
--- term3d.h
+++ term3d.h
@@ -1,38 +1,40 @@
 #ifndef TERM3D_H
-#define TERM3D_H
+# define TERM3D_H
 
-#include "vector.h"
+# include "vector.h"
 
 typedef struct s_moller
 {
-	float	kEpsilon;
+	float		kEpsilon;
 	t_vector	e1;
 	t_vector	e2;
 	t_vector	alpha;
-	float	det;
-	float	invDet;
+	float		det;
+	float		invDet;
 	t_vector	r;
 	t_vector	beta;
 }	t_moller;
 
 typedef struct s_tri
 {
-	t_vector v0;
-	t_vector v1;
-	t_vector v2;
+	t_vector	v0;
+	t_vector	v1;
+	t_vector	v2;
 }	t_tri;
 
 typedef struct s_term
 {
-	int width;
-	int height;
-	int screen_z;
-	int	zoom;
-	float	deg;
+	int			width;
+	int			height;
+	int			lim_x;
+	int			lim_y;
+	int			screen_z;
+	int			zoom;
+	float		deg;
 	t_vector	eye_pos;
 	t_vector	sphere_pos;
 	int			sphere_r;
-} t_term;
+}	t_term;
 
 #define M_PI   3.14159265358979323846264338327950288
 #define degToRad(angleInDegrees) ((angleInDegrees) * M_PI / 180.0)
@@ -40,6 +42,7 @@ typedef struct s_term
 
 #include "./include/parse_ply.h"
 float	is_colided(int x, int y, t_term *info, t_ply *ply_info);
+void	loop_draw(t_term *tm, t_ply *ply);
 int		exit_me(void);
 
 #include <stdio.h>
