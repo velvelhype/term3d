08f36e77ad0836d0780230d111190d8e525dd9c0
tyamagis @ GCML <tyamagis@student.42tokyo.jp>
2022-02-24 21:16:17 +0900

    add block over 100 chars in parse_ply

--- parse_ply.c
+++ parse_ply.c
@@ -6,7 +6,7 @@
 /*   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 14:56:58 by tyamagis          #+#    #+#             */
-/*   Updated: 2022/02/23 20:25:19 by tyamagis         ###   ########.fr       */
+/*   Updated: 2022/02/24 21:12:06 by tyamagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ void	*poly_to_tri(t_ply *ply_info, FILE *f_stream, int *read, int vtx)
 
 	new_elm = ply_info->elem_faces + vtx - 3;
 	faces = recreate_faces(ply_info, ply_info->faces, read, new_elm);
-	if (fscanf(f_stream, "%d %d %d", &faces[*read].v1, &faces[*read].v2, \
+	if (fscanf(f_stream, "%99d %99d %99d", &faces[*read].v1, &faces[*read].v2, \
 			&faces[*read].v3) != 3)
 		exit_me(ERR_FMT);
 	v1 = faces[*read].v1;
@@ -54,7 +54,7 @@ void	*poly_to_tri(t_ply *ply_info, FILE *f_stream, int *read, int vtx)
 	{
 		faces[*read].v1 = v1;
 		faces[*read].v2 = v3;
-		if (!fscanf(f_stream, "%d", &faces[*read].v3))
+		if (!fscanf(f_stream, "%99d", &faces[*read].v3))
 			exit_me(ERR_FMT);
 		v3 = faces[*read].v3;
 		(*read)++;
@@ -72,11 +72,11 @@ void	*set_faces(t_ply *ply_info, FILE *f_stream)
 	i = 0;
 	while (i < ply_info->elem_faces)
 	{
-		if (!fscanf(f_stream, "%d", &num_of_vtx))
+		if (!fscanf(f_stream, "%99d", &num_of_vtx))
 			exit_me(ERR_FMT);
 		if (num_of_vtx == 3)
 		{
-			if (fscanf(f_stream, "%d %d %d", &faces[i].v1, \
+			if (fscanf(f_stream, "%99d %99d %99d", &faces[i].v1, \
 					&faces[i].v2, &faces[i].v3) != 3)
 				exit_me(ERR_FMT);
 			i++;
@@ -102,37 +102,44 @@ void	*set_vertexes(t_ply *ply, FILE *f_stream)
 	i = 0;
 	while (i++ < ply->elem_vertexes)
 	{
-		if (fscanf(f_stream, "%f %f %f", &vtx->x, &vtx->y, &vtx->z) != 3)
+		if (fscanf(f_stream, "%99f %99f %99f", &vtx->x, &vtx->y, &vtx->z) != 3)
 			exit_me(ERR_FMT);
 		vtx++;
 	}
 	return (ply->vertexes);
 }
 
+void	check_ply_header(t_ply *p, FILE *f_stream)
+{
+	char	str[100];
+	int		ret;
+
+	ret = fscanf(f_stream, "%99s", str);
+	if (strcmp(str, "ply"))
+		exit_me(ERR_FMT);
+	while (strcmp(str, "end_header"))
+	{
+		fscanf(f_stream, "%99s", str);
+		if (strcmp(str, "vertex") == 0)
+			ret += fscanf(f_stream, "%99d", &p->elem_vertexes);
+		else if (strcmp(str, "face") == 0)
+			ret += fscanf(f_stream, "%99d", &p->elem_faces);
+	}
+	if (ret != 3)
+		exit_me(ERR_FMT);
+	return ;
+}
+
 t_ply	*parse_ply(char *filename)
 {
 	t_ply	*ply_info;
-	char	str[100];
 	FILE	*f_stream;
-	int		ret;
 
 	f_stream = fopen(filename, "r");
 	ply_info = (t_ply *)malloc(sizeof(t_ply));
 	if (f_stream == NULL || ply_info == NULL)
 		exit_me(ERR_ARG);
-	ret = fscanf(f_stream, "%s", str);
-	if (strcmp(str, "ply"))
-		exit_me(ERR_FMT);
-	while (strcmp(str, "end_header"))
-	{
-		fscanf(f_stream, "%s", str);
-		if (strcmp(str, "vertex") == 0)
-			ret += fscanf(f_stream, "%d", &ply_info->elem_vertexes);
-		else if (strcmp(str, "face") == 0)
-			ret += fscanf(f_stream, "%d", &ply_info->elem_faces);
-	}
-	if (ret != 3)
-		exit_me(ERR_FMT);
+	check_ply_header(ply_info, f_stream);
 	set_vertexes(ply_info, f_stream);
 	set_faces(ply_info, f_stream);
 	return (ply_info);
